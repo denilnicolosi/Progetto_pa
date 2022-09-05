f0d9781cbaa72f1b536721f003da167fa4a84eb1
Manuel Manelli <manuelmanelli@icloud.com>
2022-09-05 11:29:03 +0200

    model per ottenere dati utente (da ultimare)

--- app/src/models/user.ts
+++ app/src/models/user.ts
@@ -22,11 +22,22 @@ export const User = sequelize.define('users', {
 });
 
 export async function trovaTutto() {
-   // return DbConnection.getConnection();
-
-   const users = await User.findAll();
-   console.log(users.every(user => user instanceof User)); // true
-   console.log("All users:", JSON.stringify(users, null, 2));
-
-    //return await User.findAll();
-  }
+    // return DbConnection.getConnection();
+ 
+    const users = await User.findAll({
+        
+    });
+    console.log(users.every(user => user instanceof User)); // true
+    console.log("All users:", JSON.stringify(users, null, 2));
+ 
+     //return await User.findAll();
+   }
+export async function getUser(email:string) {
+    // return DbConnection.getConnection();
+ 
+    const users = await User.findAll();
+    console.log(users.every(user => user instanceof User)); // true
+    console.log("All users:", JSON.stringify(users, null, 2));
+ 
+     //return await User.findAll();
+   }
